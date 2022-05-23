c7f682ac2cbfc0ea7269a896068e127d0e178859
titan282 <nguyencongtan1002@gmail.com>
2022-05-23 09:40:30 +0700

    resolve conflict

--- app/src/main/java/ie/app/musicplayer/Application/MusicPlayerApp.java
+++ app/src/main/java/ie/app/musicplayer/Application/MusicPlayerApp.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import ie.app.musicplayer.Model.Playlist;
 import ie.app.musicplayer.Model.Song;
 import ie.app.musicplayer.R;
 
